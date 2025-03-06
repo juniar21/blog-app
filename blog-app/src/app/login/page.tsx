3ca55d4b10c18f37fe762a174b08a076286958b6
juniar21 <juniararrangbua@gmail.com>
2025-03-06 21:42:00 +0700

    Memperbaiki Login

--- blog-app/src/app/login/page.tsx
+++ blog-app/src/app/login/page.tsx
@@ -1,21 +1,14 @@
 "use client";
 import LogForm from "@/components/loginForm";
 import Navbar from "@/components/navbar";
-import Image from "next/image";
 
 export default function Login() {
   //login nanti di hapus
   return (
     <div>
       <Navbar />
-      <div className="absolute z-10 flex max-xl:hidden max-sm:w-full justify-center items-center ml-[850px]">
-        <Image src={"/login.png"} alt="error" height={300} width={400} />
-      </div>
-      <div className="absolute z-10 flex max-xl:hidden max-sm:w-full justify-center items-center ml-[350px] mt-[350px]">
-        <Image src={"/login1.png"} alt="error" height={300} width={400} />
-      </div>
-      <div className="bg-amber-400 h-screen relative flex justify-center items-center mt-[-10px]">
-        <div className="flex justify-center items-center">
+      <div className="flex h-screen w-full m-h-full justify-center items-center bg-amber-400">
+        <div className="flex justify-center items-center bg-amber-400">
           <LogForm />
         </div>
       </div>
